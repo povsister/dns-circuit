da9213804de5f3bd8e1de2184628dbadfe5cd69f
povsister <povsister@foxmail.com>
2024-03-26 19:42:16 +0800

    add packet serializer

--- ospf/packet/packet.go
+++ ospf/packet/packet.go
@@ -1,18 +1,319 @@
 package packet
 
 import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 )
 
-// OSPFv2 wrapper implements SerializeTo.
+// OSPFv2Packet wrapper implements SerializeTo.
 // Make it possible to serialize an OSPF message to wire format
-type OSPFv2 struct {
+type OSPFv2Packet[T OSPFPayloadV2] struct {
 	layers.OSPFv2
+	Payload T //replace Content interface for marshaling
+}
+
+type OSPFPayloadV2 interface {
+	HelloPayloadV2 | DbDescPayload |
+		LSReqPayload | LSUpdatePayload | LSAcknowledgementPayload
+	marshalable
+}
+
+var (
+	ErrBufferLengthTooShort = errors.New("err buffer length too short")
+)
+
+type HelloPayloadV2 layers.HelloPkgV2
+
+func (p HelloPayloadV2) Size() int {
+	return 20 + 4*len(p.NeighborID)
+}
+
+func (p HelloPayloadV2) SerializeToSizedBuffer(b []byte) error {
+	if len(b) < p.Size() {
+		return ErrBufferLengthTooShort
+	}
+	binary.BigEndian.PutUint32(b[0:4], p.NetworkMask)
+	binary.BigEndian.PutUint16(b[4:6], p.HelloInterval)
+	b[6] = uint8(p.Options)
+	b[7] = p.RtrPriority
+	binary.BigEndian.PutUint32(b[8:12], p.RouterDeadInterval)
+	binary.BigEndian.PutUint32(b[12:16], p.DesignatedRouterID)
+	binary.BigEndian.PutUint32(b[16:20], p.BackupDesignatedRouterID)
+	for idx, neighbor := range p.NeighborID {
+		binary.BigEndian.PutUint32(b[20+idx*4:20+(idx+1)*4], neighbor)
+	}
+	return nil
+}
+
+type HelloPayloadV3 layers.HelloPkg
+
+type DbDescPayload layers.DbDescPkg
+
+func (p DbDescPayload) Size() int {
+	return 8 + LSAheader{}.Size()*len(p.LSAinfo)
+}
+
+func (p DbDescPayload) SerializeToSizedBuffer(b []byte) (err error) {
+	if len(b) < p.Size() {
+		return ErrBufferLengthTooShort
+	}
+	binary.BigEndian.PutUint16(b[0:2], p.InterfaceMTU)
+	b[2] = uint8(p.Options)
+	b[3] = uint8(p.Flags)
+	binary.BigEndian.PutUint32(b[4:8], p.DDSeqNumber)
+	for idx, lsaH := range p.LSAinfo {
+		thisLSAh := LSAheader(lsaH)
+		if err = thisLSAh.SerializeToSizedBuffer(b[8+idx*thisLSAh.Size() : 8+(idx+1)*thisLSAh.Size()]); err != nil {
+			return
+		}
+	}
+	return
+}
+
+type LSReqPayload []LSRequest
+
+func (p LSReqPayload) Size() int {
+	total := 0
+	for _, eachP := range p {
+		total += eachP.Size()
+	}
+	return total
+}
+
+func (p LSReqPayload) SerializeToSizedBuffer(b []byte) (err error) {
+	if len(b) < p.Size() {
+		return ErrBufferLengthTooShort
+	}
+	offset := 0
+	for _, eachP := range p {
+		if err = eachP.SerializeToSizedBuffer(b[offset : offset+eachP.Size()]); err != nil {
+			return
+		}
+		offset += eachP.Size()
+	}
+	return
+}
+
+type LSRequest layers.LSReq
+
+func (p LSRequest) Size() int {
+	return 12
+}
+
+func (p LSRequest) SerializeToSizedBuffer(b []byte) (err error) {
+	if len(b) < p.Size() {
+		return ErrBufferLengthTooShort
+	}
+	binary.BigEndian.PutUint32(b[0:4], uint32(p.LSType))
+	binary.BigEndian.PutUint32(b[4:8], p.LSID)
+	binary.BigEndian.PutUint32(b[8:12], p.AdvRouter)
+	return
+}
+
+type LSUpdatePayload struct {
+	layers.LSUpdate
+	LSAs []LSAdvertisement
+}
+
+func (p LSUpdatePayload) Size() int {
+	totalLen := 4
+	for _, l := range p.LSAs {
+		totalLen += l.Size()
+	}
+	return totalLen
+}
+
+func (p LSUpdatePayload) SerializeToSizedBuffer(b []byte) (err error) {
+	if len(b) < p.Size() {
+		return ErrBufferLengthTooShort
+	}
+	binary.BigEndian.PutUint32(b[0:4], p.NumOfLSAs)
+	offset := 0
+	for _, l := range p.LSAs {
+		if err = l.SerializeToSizedBuffer(b[4+offset : 4+offset+l.Size()]); err != nil {
+			return
+		}
+		offset += l.Size()
+	}
+	return
+}
+
+type LSAcknowledgementPayload []LSAheader
+
+func (p LSAcknowledgementPayload) Size() int {
+	return LSAheader{}.Size() * len(p)
+}
+
+func (p LSAcknowledgementPayload) SerializeToSizedBuffer(b []byte) (err error) {
+	if len(b) < p.Size() {
+		return ErrBufferLengthTooShort
+	}
+	offset := 0
+	for _, h := range p {
+		if err = h.SerializeToSizedBuffer(b[offset : offset+h.Size()]); err != nil {
+			return
+		}
+		offset += h.Size()
+	}
+	return
+}
+
+type LSAdvertisement struct {
+	layers.LSA
+	Content LSAContent
+}
+
+func (p LSAdvertisement) Size() int {
+	if p.Length > 0 {
+		return int(p.Length)
+	}
+	return LSAheader(p.LSAheader).Size() + p.Content.Size()
+}
+
+func (p LSAdvertisement) SerializeToSizedBuffer(b []byte) (err error) {
+	lsaH := LSAheader(p.LSAheader)
+	if len(b) < p.Size() || len(b) < lsaH.Size() || len(b) < p.Content.Size() {
+		return ErrBufferLengthTooShort
+	}
+
+	// fix length
+	if lsaH.Length <= 0 {
+		lsaH.Length = uint16(lsaH.Size() + p.Content.Size())
+	}
+	if err = lsaH.SerializeToSizedBuffer(b[0:lsaH.Size()]); err != nil {
+		return
+	}
+	if err = p.Content.SerializeToSizedBuffer(b[lsaH.Size() : lsaH.Size()+p.Content.Size()]); err != nil {
+		return
+	}
+	// fix chksum
+	if p.LSChecksum <= 0 {
+		lsaH.recalculateChecksum(b)
+	}
+	return
+}
+
+// Fletcher-16 checksum
+func lsaChecksum(b []byte) uint16 {
+	var (
+		sum1, sum2 uint16 = 0, 0
+	)
+	for i := 0; i < len(b); i++ {
+		sum1 = (sum1 + uint16(b[i])) % 255
+		sum2 = (sum2 + sum1) % 255
+	}
+	return (sum2 << 8) | sum1
+}
+
+type LSAheader layers.LSAheader
+
+func (p LSAheader) Size() int {
+	return 20
+}
+
+func (p LSAheader) recalculateChecksum(b []byte) {
+	// clear LS age before calculation
+	clear(b[0:2])
+	// clear chksum bytes
+	clear(b[16:18])
+	binary.BigEndian.PutUint16(b[16:18], lsaChecksum(b))
+	// add back LS age
+	binary.BigEndian.PutUint16(b[0:2], p.LSAge)
+}
+
+func (p LSAheader) SerializeToSizedBuffer(b []byte) error {
+	if len(b) < p.Size() {
+		return ErrBufferLengthTooShort
+	}
+	binary.BigEndian.PutUint16(b[0:2], p.LSAge)
+	b[2] = p.LSOptions
+	b[3] = uint8(p.LSType)
+	binary.BigEndian.PutUint32(b[4:8], p.LinkStateID)
+	binary.BigEndian.PutUint32(b[8:12], p.AdvRouter)
+	binary.BigEndian.PutUint32(b[12:16], p.LSSeqNumber)
+	binary.BigEndian.PutUint16(b[16:18], p.LSChecksum)
+	binary.BigEndian.PutUint16(b[18:20], p.Length)
+	return nil
+}
+
+type marshalable interface {
+	Size() int
+	SerializeToSizedBuffer(b []byte) error
+}
+
+type LSAContent interface {
+	isLSAContent() // make it private
+	marshalable
+}
+
+func (v2 *OSPFv2Packet[T]) packetErr(format string, args ...interface{}) error {
+	return fmt.Errorf("malformed ospfv2 %s packet: "+format, append([]interface{}{v2.Type}, args...)...)
+}
+
+func ipPacketChecksum(bytes []byte) uint16 {
+	// Compute ipPacketChecksum
+	var csum uint32
+	for i := 0; i < len(bytes); i += 2 {
+		csum += uint32(bytes[i]) << 8
+		csum += uint32(bytes[i+1])
+	}
+	for {
+		// Break when sum is less or equals to 0xFFFF
+		if csum <= 65535 {
+			break
+		}
+		// Add carry to the sum
+		csum = (csum >> 16) + uint32(uint16(csum))
+	}
+	// Flip all the bits
+	return ^uint16(csum)
 }
 
 // SerializeTo writes the serialized form of this layer into the
 // SerializationBuffer, implementing gopacket.SerializableLayer.
-func (v2 *OSPFv2) SerializeTo(b gopacket.SerializeBuffer, opts gopacket.SerializeOptions) error {
-	return nil
+func (v2 *OSPFv2Packet[T]) SerializeTo(b gopacket.SerializeBuffer, opts gopacket.SerializeOptions) (err error) {
+	// proc header at last
+	defer func() {
+		if err != nil {
+			// some err occurred before, simply return it
+			return
+		}
+		var h []byte
+		h, err = b.PrependBytes(24) // per RFC2328 A.3.1
+		if err != nil {
+			return
+		}
+		// header manipulation
+		h[0] = v2.Version
+		h[1] = uint8(v2.Type)
+		if opts.FixLengths {
+			v2.PacketLength = uint16(len(b.Bytes()))
+		}
+		binary.BigEndian.PutUint16(h[2:4], v2.PacketLength)
+		binary.BigEndian.PutUint32(h[4:8], v2.RouterID)
+		binary.BigEndian.PutUint32(h[8:12], v2.AreaID)
+		binary.BigEndian.PutUint16(h[14:16], v2.AuType)
+		if opts.ComputeChecksums {
+			// clear ipPacketChecksum bytes
+			clear(h[12:14])
+			// clear authentication bytes
+			clear(h[16:24])
+			v2.Checksum = ipPacketChecksum(b.Bytes())
+		}
+		binary.BigEndian.PutUint16(h[12:14], v2.Checksum)
+		// ipPacketChecksum calculation must exclude 64bit authentication bytes.
+		// so make it last set.
+		binary.BigEndian.PutUint64(h[16:24], v2.Authentication)
+	}()
+
+	// proc payload first
+	p, err := b.AppendBytes(v2.Payload.Size())
+	if err != nil {
+		return
+	}
+	return v2.Payload.SerializeToSizedBuffer(p)
 }
